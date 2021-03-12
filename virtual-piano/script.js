0b1a26f9c6eb4ea965b788f913ac9c6f4d5b18d3
tkachiko <tkachikos@gmail.com>
2021-03-12 22:34:30 +0300

    feat: implement audio playback on mousedown event in script.js

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const piano = document.querySelector(".piano");
 const pianoKeys = document.querySelectorAll(".piano-key");
 const fullscreen = document.querySelector(".fullscreen");
@@ -89,3 +90,27 @@ fullscreen.addEventListener("click", togglefullscreen);
 
 btnNotes.addEventListener("click", switchToNotes);
 btnLetters.addEventListener("click", switchToLetters);
+=======
+const piano = document.querySelector('.piano')
+const pianoKeys = document.querySelectorAll('.piano-key')
+
+pianoKeys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+piano.addEventListener('mousedown', playAudio)
+
+function playAudio(event) {
+    const note = event.target.dataset.note
+    const audio = document.querySelector(`audio[data-note="${note}"]`)
+    const key = document.querySelector(`.piano-key[data-note="${note}"]`)
+    audio.currentTime = 0
+    audio.play();
+
+    key.classList.add('piano-key-active-pseudo', 'piano-key-active');
+}
+
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return
+
+    this.classList.remove('piano-key-active-pseudo', 'piano-key-active')
+}
+>>>>>>> feat: implement audio playback on mousedown event in script.js

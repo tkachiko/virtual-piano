ea78b388f6894d77e08dc3a95781bc4dcdfa573a
tkachiko <tkachikos@gmail.com>
2021-03-13 19:03:37 +0300

    feat: implement switching between notes/letters modes in script.js

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,12 +1,28 @@
 const piano = document.querySelector('.piano');
 const pianoKeys = document.querySelectorAll('.piano-key');
 const fullscreen = document.querySelector('.fullscreen');
+const btnNotes = document.querySelector('.btn-notes')
+const btnLetters = document.querySelector('.btn-letters')
 
 pianoKeys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 
+btnNotes.addEventListener('click', switchToNotes)
+btnLetters.addEventListener('click', switchToLetters)
 fullscreen.addEventListener('click', togglefullscreen);
 piano.addEventListener('mousedown', playAudio);
 window.addEventListener('keydown', playOnKey);
+
+function switchToLetters() {
+    btnLetters.classList.add('btn-active')
+    pianoKeys.forEach(key => key.classList.add('letter'))
+    btnNotes.classList.remove('btn-active')
+}
+
+function switchToNotes() {
+    btnNotes.classList.add('btn-active')
+    pianoKeys.forEach(key => key.classList.remove('letter'))
+    btnLetters.classList.remove('btn-active')
+}
 
 function playAudio(event) {
     const note = event.target.dataset.note;

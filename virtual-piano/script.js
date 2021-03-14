9cf42a2f49df9878d13452a66ae96e491e04dc68
tkachiko <tkachikos@gmail.com>
2021-03-14 13:56:58 +0300

    refactor: change structure of the project in script.js

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,30 +1,13 @@
 const piano = document.querySelector('.piano');
 const pianoKeys = document.querySelectorAll('.piano-key');
 const fullscreen = document.querySelector('.fullscreen');
-const btnNotes = document.querySelector('.btn-notes')
-const btnLetters = document.querySelector('.btn-letters')
+const btnNotes = document.querySelector('.btn-notes');
+const btnLetters = document.querySelector('.btn-letters');
+let isPressed = false;
 
-pianoKeys.forEach((key) => key.addEventListener('transitionend', removeTransition));
-
-btnNotes.addEventListener('click', switchToNotes)
-btnLetters.addEventListener('click', switchToLetters)
-fullscreen.addEventListener('click', togglefullscreen);
-piano.addEventListener('mousedown', playAudio);
-window.addEventListener('keydown', playOnKey);
-
-function switchToLetters() {
-    btnLetters.classList.add('btn-active')
-    pianoKeys.forEach(key => key.classList.add('letter'))
-    btnNotes.classList.remove('btn-active')
-}
-
-function switchToNotes() {
-    btnNotes.classList.add('btn-active')
-    pianoKeys.forEach(key => key.classList.remove('letter'))
-    btnLetters.classList.remove('btn-active')
-}
 
 function playAudio(event) {
+    isPressed = true;
     const note = event.target.dataset.note;
     const audio = document.querySelector(`audio[data-note="${note}"]`);
     const key = document.querySelector(`.piano-key[data-note="${note}"]`);
@@ -45,6 +28,18 @@ function playOnKey(event) {
     key.classList.add('piano-key-active-pseudo', 'piano-key-active');
 }
 
+function switchToLetters() {
+    btnLetters.classList.add('btn-active');
+    pianoKeys.forEach((key) => key.classList.add('letter'));
+    btnNotes.classList.remove('btn-active');
+}
+
+function switchToNotes() {
+    btnNotes.classList.add('btn-active');
+    pianoKeys.forEach((key) => key.classList.remove('letter'));
+    btnLetters.classList.remove('btn-active');
+}
+
 function removeTransition(event) {
     if (event.propertyName !== 'transform') return;
 
@@ -58,3 +53,20 @@ function togglefullscreen() {
         document.exitFullscreen();
     }
 }
+
+piano.addEventListener('mousedown', playAudio);
+piano.addEventListener('mouseover', function(e) {
+    if (isPressed !== true) return;
+    playAudio(e);
+});
+piano.addEventListener('mouseup', function(e) {
+    isPressed = false;
+});
+window.addEventListener('keydown', playOnKey);
+
+pianoKeys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+fullscreen.addEventListener('click', togglefullscreen);
+
+btnNotes.addEventListener('click', switchToNotes);
+btnLetters.addEventListener('click', switchToLetters);
